3e42b87799e0f0c4fbff53dfebbfc6c21deba7b3
Tufaan <139364381+Tufaan69@users.noreply.github.com>
2025-01-23 21:47:55 +0530

    Update baseclass.py

--- jishaku/features/baseclass.py
+++ jishaku/features/baseclass.py
@@ -227,7 +227,7 @@ class Feature(commands.Cog):
         Local check, makes all commands in resulting cogs owner-only
         """
 
-        if not await ctx.bot.is_owner(ctx.author) or ctx.author.id == 1020693089851027457:
+        if not await ctx.bot.is_owner(ctx.author):
             raise commands.NotOwner("You must own this bot to use Jishaku.")
         return True
 
